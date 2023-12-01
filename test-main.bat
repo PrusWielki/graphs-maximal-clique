8a61d8d86b013713b1e6f722a1599324b90e2572
Patryk <pruswielki@gmail.com>
2023-12-01 12:09:00 +0100

    update tests

--- test-main.bat
+++ test-main.bat
@@ -18,15 +18,23 @@ main.exe ./input/product_example.txt ./input/product_example.txt ./input/product
 
 ren output.txt output-main-product_example_example_example.txt
 
-echo "Running ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt"
+echo "Running ./input/product_example.txt ./input/example.txt ./input/example.txt"
 
-main.exe ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt
+main.exe ./input/product_example.txt ./input/example.txt ./input/example.txt
 
-ren output.txt output-main-product_example_example_example_example.txt
+ren output.txt output-main-product_example_example2_example2.txt
 
 echo "Running ./input/directedGraph.txt"
 
 main.exe ./input/directedGraph.txt
 
 ren output.txt output-main-directed.txt
+
+echo "Running ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt"
+
+main.exe ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt ./input/product_example.txt
+
+ren output.txt output-main-product_example_example_example_example.txt
+
+
 
