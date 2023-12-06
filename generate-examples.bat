05b9278505a750aa63f90b2e7038f6d0f51eb9a8
Patryk <pruswielki@gmail.com>
2023-12-06 10:32:06 +0100

    add testing suite

---
+++ generate-examples.bat
@@ -0,0 +1,14 @@
+python graphMaker.py 10 10
+python graphMaker.py 20 10
+python graphMaker.py 50 10
+python graphMaker.py 100 10
+
+python graphMaker.py 10 15
+python graphMaker.py 20 15
+python graphMaker.py 50 15
+python graphMaker.py 100 15
+
+python graphMaker.py 10 20
+python graphMaker.py 20 20
+python graphMaker.py 50 20
+python graphMaker.py 100 20
---
+++ run-tests.bat
@@ -0,0 +1,46 @@
+gcc -m64 -static -o main main.c
+
+main.exe Graph10_10.txt
+ren output.txt output-Graph10_10.txt
+
+main.exe Graph20_10.txt
+ren output.txt output-Graph20_10.txt
+
+main.exe Graph50_10.txt
+ren output.txt output-Graph50_10.txt
+
+main.exe Graph100_10.txt
+ren output.txt output-Graph100_10.txt
+
+
+
+
+
+
+main.exe Graph10_15.txt
+ren output.txt output-Graph10_15.txt
+
+main.exe Graph20_15.txt
+ren output.txt output-Graph20_15.txt
+
+main.exe Graph50_15.txt
+ren output.txt output-Graph50_15.txt
+
+main.exe Graph100_15.txt
+ren output.txt output-Graph100_15.txt
+
+
+
+
+
+main.exe Graph10_20.txt
+ren output.txt output-Graph10_20.txt
+
+main.exe Graph20_20.txt
+ren output.txt output-Graph20_20.txt
+
+main.exe Graph50_20.txt
+ren output.txt output-Graph50_20.txt
+
+main.exe Graph100_20.txt
+ren output.txt output-Graph100_20.txt
