57bc53fca7e9d9fb0eb59c83e13242f4ab2b7af8
Patryk <pruswielki@gmail.com>
2023-12-07 16:43:38 +0100

    update tests

--- generate-examples.bat
+++ generate-examples.bat
@@ -42,44 +42,71 @@ python graphMaker.py 1 700
 python graphMaker.py 1 800
 
 
-python multiGraphMaker.py 1 5
-python multiGraphMaker.py 2 5
-python multiGraphMaker.py 5 5
+python multiGraphMaker.py 1 10 5
+python multiGraphMaker.py 2 10 5
+python multiGraphMaker.py 5 10 5
 
-python multiGraphMaker.py 1 10
-python multiGraphMaker.py 2 10
-python multiGraphMaker.py 5 10
+python multiGraphMaker.py 1 10 10
+python multiGraphMaker.py 2 10 10
+python multiGraphMaker.py 5 10 10
 
 
 
-python multiGraphMaker.py 1 11
-python multiGraphMaker.py 2 11
-python multiGraphMaker.py 5 11
+python multiGraphMaker.py 1 10 11
+python multiGraphMaker.py 2 10 11
+python multiGraphMaker.py 5 10 11
 
-python multiGraphMaker.py 1 12
-python multiGraphMaker.py 2 12
-python multiGraphMaker.py 5 12
+python multiGraphMaker.py 1 10 12
+python multiGraphMaker.py 2 10 12
+python multiGraphMaker.py 5 10 12
 
-python multiGraphMaker.py 1 13
-python multiGraphMaker.py 2 13
-python multiGraphMaker.py 5 13
+python multiGraphMaker.py 1 10 13
+python multiGraphMaker.py 2 10 13
+python multiGraphMaker.py 5 10 13
 
-python multiGraphMaker.py 1 14
-python multiGraphMaker.py 2 14
-python multiGraphMaker.py 5 14
+python multiGraphMaker.py 1 10 14
+python multiGraphMaker.py 2 10 14
+python multiGraphMaker.py 5 10 14
 
-python multiGraphMaker.py 1 100
+python multiGraphMaker.py 1 10 100
 
-python multiGraphMaker.py 1 200
+python multiGraphMaker.py 1 10 200
 
-python multiGraphMaker.py 1 300
+python multiGraphMaker.py 1 10 300
 
-python multiGraphMaker.py 1 400
+python multiGraphMaker.py 1 10 400
 
-python multiGraphMaker.py 1 500
+python multiGraphMaker.py 1 10 500
 
-python multiGraphMaker.py 1 600
+python multiGraphMaker.py 1 10 600
 
-python multiGraphMaker.py 1 700
+python multiGraphMaker.py 1 10 700
 
-python multiGraphMaker.py 1 800
+python multiGraphMaker.py 1 10 800
+
+python multiGraphMaker.py 1 5 5
+python multiGraphMaker.py 2 5 5
+python multiGraphMaker.py 5 5 5
+python multiGraphMaker.py 1 5 10
+python multiGraphMaker.py 2 5 10
+python multiGraphMaker.py 5 5 10
+python multiGraphMaker.py 1 5 11
+python multiGraphMaker.py 2 5 11
+python multiGraphMaker.py 5 5 11
+python multiGraphMaker.py 1 5 12
+python multiGraphMaker.py 2 5 12
+python multiGraphMaker.py 5 5 12
+python multiGraphMaker.py 1 5 13
+python multiGraphMaker.py 2 5 13
+python multiGraphMaker.py 5 5 13
+python multiGraphMaker.py 1 5 14
+python multiGraphMaker.py 2 5 14
+python multiGraphMaker.py 5 5 14
+python multiGraphMaker.py 1 5 100
+python multiGraphMaker.py 1 5 200
+python multiGraphMaker.py 1 5 300
+python multiGraphMaker.py 1 5 400
+python multiGraphMaker.py 1 5 500
+python multiGraphMaker.py 1 5 600
+python multiGraphMaker.py 1 5 700
+python multiGraphMaker.py 1 5 800
--- run-tests.bat
+++ run-tests.bat
@@ -76,72 +76,143 @@ ren output.txt output-Graph5_14.txt
 
 
 
-main.exe MultiGraph1_5.txt
-ren output.txt output-MultiGraph1_5.txt
+main.exe MultiGraph1_10_5.txt
+ren output.txt output-MultiGraph1_10_5.txt
 
-main.exe MultiGraph2_5.txt
-ren output.txt output-MultiGraph2_5.txt
+main.exe MultiGraph2_10_5.txt
+ren output.txt output-MultiGraph2_10_5.txt
 
-main.exe MultiGraph5_5.txt
-ren output.txt output-MultiGraph5_5.txt
-
-
+main.exe MultiGraph5_10_5.txt
+ren output.txt output-MultiGraph5_10_5.txt
 
 
 
 
 
 
-main.exe MultiGraph1_10.txt
-ren output.txt output-MultiGraph1_10.txt
-
-main.exe MultiGraph2_10.txt
-ren output.txt output-MultiGraph2_10.txt
-
-main.exe MultiGraph5_10.txt
-ren output.txt output-MultiGraph5_10.txt
 
 
+main.exe MultiGraph1_10_10.txt
+ren output.txt output-MultiGraph1_10_10.txt
+
+main.exe MultiGraph2_10_10.txt
+ren output.txt output-MultiGraph2_10_10.txt
+
+main.exe MultiGraph5_10_10.txt
+ren output.txt output-MultiGraph5_10_10.txt
 
 
 
-main.exe MultiGraph1_11.txt
-ren output.txt output-MultiGraph1_11.txt
-
-main.exe MultiGraph2_11.txt
-ren output.txt output-MultiGraph2_11.txt
-
-main.exe MultiGraph5_11.txt
-ren output.txt output-MultiGraph5_11.txt
 
 
-main.exe MultiGraph1_12.txt
-ren output.txt output-MultiGraph1_12.txt
+main.exe MultiGraph1_10_11.txt
+ren output.txt output-MultiGraph1_10_11.txt
 
-main.exe MultiGraph2_12.txt
-ren output.txt output-MultiGraph2_12.txt
+main.exe MultiGraph2_10_11.txt
+ren output.txt output-MultiGraph2_10_11.txt
 
-main.exe MultiGraph5_12.txt
-ren output.txt output-MultiGraph5_12.txt
-
-main.exe MultiGraph1_13.txt
-ren output.txt output-MultiGraph1_13.txt
-
-main.exe MultiGraph2_13.txt
-ren output.txt output-MultiGraph2_13.txt
-
-main.exe MultiGraph5_13.txt
-ren output.txt output-MultiGraph5_13.txt
+main.exe MultiGraph5_10_11.txt
+ren output.txt output-MultiGraph5_10_11.txt
 
 
-main.exe MultiGraph1_14.txt
-ren output.txt output-MultiGraph1_14.txt
+main.exe MultiGraph1_10_12.txt
+ren output.txt output-MultiGraph1_10_12.txt
 
-main.exe MultiGraph2_14.txt
-ren output.txt output-MultiGraph2_14.txt
+main.exe MultiGraph2_10_12.txt
+ren output.txt output-MultiGraph2_10_12.txt
 
-main.exe MultiGraph5_14.txt
-ren output.txt output-MultiGraph5_14.txt
+main.exe MultiGraph5_10_12.txt
+ren output.txt output-MultiGraph5_10_12.txt
+
+main.exe MultiGraph1_10_13.txt
+ren output.txt output-MultiGraph1_10_13.txt
+
+main.exe MultiGraph2_10_13.txt
+ren output.txt output-MultiGraph2_10_13.txt
+
+main.exe MultiGraph5_10_13.txt
+ren output.txt output-MultiGraph5_10_13.txt
+
+
+main.exe MultiGraph1_10_14.txt
+ren output.txt output-MultiGraph1_10_14.txt
+
+main.exe MultiGraph2_10_14.txt
+ren output.txt output-MultiGraph2_10_14.txt
+
+main.exe MultiGraph5_10_14.txt
+ren output.txt output-MultiGraph5_10_14.txt
+
+
+
+
+main.exe MultiGraph1_10_5.txt
+ren output.txt output-MultiGraph1_10_5.txt
+
+main.exe MultiGraph2_10_5.txt
+ren output.txt output-MultiGraph2_10_5.txt
+
+main.exe MultiGraph5_10_5.txt
+ren output.txt output-MultiGraph5_10_5.txt
+
+
+
+
+
+
+
+
+main.exe MultiGraph1_5_10.txt
+ren output.txt output-MultiGraph1_5_10.txt
+
+main.exe MultiGraph2_5_10.txt
+ren output.txt output-MultiGraph2_5_10.txt
+
+main.exe MultiGraph5_5_10.txt
+ren output.txt output-MultiGraph5_5_10.txt
+
+
+
+
+
+main.exe MultiGraph1_5_11.txt
+ren output.txt output-MultiGraph1_5_11.txt
+
+main.exe MultiGraph2_5_11.txt
+ren output.txt output-MultiGraph2_5_11.txt
+
+main.exe MultiGraph5_5_11.txt
+ren output.txt output-MultiGraph5_5_11.txt
+
+
+main.exe MultiGraph1_5_12.txt
+ren output.txt output-MultiGraph1_5_12.txt
+
+main.exe MultiGraph2_5_12.txt
+ren output.txt output-MultiGraph2_5_12.txt
+
+main.exe MultiGraph5_5_12.txt
+ren output.txt output-MultiGraph5_5_12.txt
+
+main.exe MultiGraph1_5_13.txt
+ren output.txt output-MultiGraph1_5_13.txt
+
+main.exe MultiGraph2_5_13.txt
+ren output.txt output-MultiGraph2_5_13.txt
+
+main.exe MultiGraph5_5_13.txt
+ren output.txt output-MultiGraph5_5_13.txt
+
+
+main.exe MultiGraph1_5_14.txt
+ren output.txt output-MultiGraph1_5_14.txt
+
+main.exe MultiGraph2_5_14.txt
+ren output.txt output-MultiGraph2_5_14.txt
+
+main.exe MultiGraph5_5_14.txt
+ren output.txt output-MultiGraph5_5_14.txt
+
 
 
 
@@ -151,63 +222,63 @@ main.exe Graph1_100.txt
 ren output.txt output-Graph1_100.txt
 
 
-main.exe MultiGraph1_100.txt
-ren output.txt output-MultiGraph1_100.txt
+main.exe MultiGraph1_10_100.txt
+ren output.txt output-MultiGraph1_10_100.txt
 
 
 main.exe Graph1_200.txt
 ren output.txt output-Graph1_200.txt
 
 
-main.exe MultiGraph1_200.txt
-ren output.txt output-MultiGraph1_200.txt
+main.exe MultiGraph1_10_200.txt
+ren output.txt output-MultiGraph1_10_200.txt
 
 main.exe Graph1_300.txt
 ren output.txt output-Graph1_300.txt
 
 
-main.exe MultiGraph1_300.txt
-ren output.txt output-MultiGraph1_300.txt
+main.exe MultiGraph1_10_300.txt
+ren output.txt output-MultiGraph1_10_300.txt
 
 
 main.exe Graph1_400.txt
 ren output.txt output-Graph1_400.txt
 
 
-main.exe MultiGraph1_400.txt
-ren output.txt output-MultiGraph1_400.txt
+main.exe MultiGraph1_10_400.txt
+ren output.txt output-MultiGraph1_10_400.txt
 
 
 main.exe Graph1_500.txt
 ren output.txt output-Graph1_500.txt
 
 
-main.exe MultiGraph1_500.txt
-ren output.txt output-MultiGraph1_500.txt
+main.exe MultiGraph1_10_500.txt
+ren output.txt output-MultiGraph1_10_500.txt
 
 
 main.exe Graph1_600.txt
 ren output.txt output-Graph1_600.txt
 
 
-main.exe MultiGraph1_600.txt
-ren output.txt output-MultiGraph1_600.txt
+main.exe MultiGraph1_10_600.txt
+ren output.txt output-MultiGraph1_10_600.txt
 
 
 main.exe Graph1_700.txt
 ren output.txt output-Graph1_700.txt
 
 
-main.exe MultiGraph1_700.txt
-ren output.txt output-MultiGraph1_700.txt
+main.exe MultiGraph1_10_700.txt
+ren output.txt output-MultiGraph1_10_700.txt
 
 
 main.exe Graph1_800.txt
 ren output.txt output-Graph1_800.txt
 
 
-main.exe MultiGraph1_800.txt
-ren output.txt output-MultiGraph1_800.txt
+main.exe MultiGraph1_10_800.txt
+ren output.txt output-MultiGraph1_10_800.txt
 
  
 
